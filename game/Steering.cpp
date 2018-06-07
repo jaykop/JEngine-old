fe7dcb790710f24fecee3b1e3814605ec3310e22
jaykop <jeykop14@gmail.com>
2018-06-07 18:06:43 +0900

    Working on wandering behavior.

--- game/Steering.cpp
+++ game/Steering.cpp
@@ -4,101 +4,21 @@
 
 jeBegin
 
-vec3 Steering::Seek(const vec3& _targetPos)
+// Helper functions
+
+void Truncate(vec3& vector, float _max)
 {
-    if (GetOwner()->HasComponent<Transform>()) {
-        Transform *transform = GetOwner()->GetComponent<Transform>();
-        vec3 desiredVel = (_targetPos - transform->m_position) * maxSpeed;
-
-        return desiredVel - velocity;
-    }
-
-    return vec3::ZERO;
-}
-
-vec3 Steering::Flee(const vec3& _targetPos)
-{
-	if (GetOwner()->HasComponent<Transform>()) {
-		Transform *transform = GetOwner()->GetComponent<Transform>();
-		vec3 desiredVel = (transform->m_position - _targetPos) * maxSpeed;
-
-		return desiredVel - velocity;
+	if (vector.GetLength() > _max) {
+		vector.Normalize();
+		vector *= _max;
 	}
-	return vec3::ZERO;
-}
-
-vec3 Steering::Calculate()
-{
-    vec3 force;
-
-    // case: Seek
-    force = Seek(m_target->GetComponent<Transform>()->m_position) /** 1.f*/;
-
-    if (!AccumulateForce(force)) 
-		return steeringForce;
-
-	return steeringForce;
-}
-
-bool Steering::AccumulateForce(const vec3& forceToAdd)
-{
-	// Get the current magnitude
-    float magnitudeSoFar = steeringForce.GetLength();
-
-	// Get the remaining magnitude
-    float magnitudeRemaining = maxForce - magnitudeSoFar;
-
-	// If there is no more remaining magnitude to add,
-	// return false
-    if (magnitudeRemaining <= 0.f) return false;
-
-	// Otherwise, get force to add
-    float magnitudeToAdd = forceToAdd.GetLength();
-
-	// If that valuse is lower than capable remaining magnitude,
-	// just add it
-    if (magnitudeToAdd < magnitudeRemaining)
-		steeringForce = forceToAdd;
-
-	// If not, modify its length and add it
-    else
-		steeringForce = forceToAdd.GetNormalize() * maxForce;
-
-    return true;
-}
-
-    Steering::Steering(Object* _pObject)
-    :CustomComponent(_pObject)
-{}
-
-void Steering::Register()
-{
-    SYSTEM::GetBehaviorSystem()->AddBehavior(this);
-}
-
-void Steering::Load(CR_RJValue /*_data*/)
-{}
-
-void Steering::Init()
-{
-    m_target = CONTAINER->GetObject("Target");
-    mass = 1.f;
-    maxSpeed = 50.f;
-	maxForce = 250.f;
-	velocity.SetZero();
-}
-
-void Truncate(float& value, float max)
-{
-    if (max < value)
-        value = max;
 }
 
 void ControlPosition(vec3& _position)
 {
 	const float halfWidth = SYSTEM::GetGraphicSystem()->GetWidth() / 2.f;
 	const float halfHeight = SYSTEM::GetGraphicSystem()->GetHeight() / 2.f;
-	
+
 	if (_position.x > halfWidth)
 		_position.x = -halfWidth;
 
@@ -113,49 +33,98 @@ void ControlPosition(vec3& _position)
 
 }
 
+Steering::Steering(Object* _pObject)
+    :CustomComponent(_pObject)
+{}
+
+void Steering::Register()
+{
+    SYSTEM::GetBehaviorSystem()->AddBehavior(this);
+}
+
+void Steering::Load(CR_RJValue _data)
+{
+	if (!strcmp(_data["Behavior"].GetString(), "Seek"))
+		m_behavior = seek;
+	else if (!strcmp(_data["Behavior"].GetString(), "Flee"))
+		m_behavior = flee;
+	else if (!strcmp(_data["Behavior"].GetString(), "Arrive"))
+		m_behavior = arrive;
+	else if (!strcmp(_data["Behavior"].GetString(), "Pursuit"))
+		m_behavior = pursuit;
+	else if (!strcmp(_data["Behavior"].GetString(), "Evade"))
+		m_behavior = evade;
+	else if (!strcmp(_data["Behavior"].GetString(), "Wander"))
+		m_behavior = wander;
+}
+
+void Steering::Init()
+{
+	if (m_behavior == pursuit)
+		m_evader = CONTAINER->GetObject("Evader")->GetComponent<Steering>();
+	else if (m_behavior == evade)
+		m_pursuer = CONTAINER->GetObject("Pursuer")->GetComponent<Steering>();
+
+	// Get target transform
+    m_target = CONTAINER->GetObject("Target");
+	targetTransform = m_target->GetComponent<Transform>();
+	
+	// Get owner's transform
+	m_transform = GetOwner()->GetComponent<Transform>();
+
+
+	mass = 1.f;
+    maxSpeed = 50.f;
+	maxForce = 250.f;
+	heading.SetUnitX();
+	deceleration = 2.f;
+
+	wanderTarget.SetZero();
+	velocity.SetZero();
+}
+
+vec3 GetPerpendicular(const vec3& vector)
+{
+	vec3 result;
+	result.x = -vector.y;
+	result.y = vector.x;
+
+	return result;
+}
+
 void Steering::Update(const float _dt)
 {
-	Transform* targetTransform = nullptr;
-	if (m_target->GetComponent<Transform>())
-		targetTransform = m_target->GetComponent<Transform>();
-
+	// By mouse
 	if (INPUT::KeyTriggered(JE_MOUSE_LEFT))
-		m_target->GetComponent<Transform>()->m_position.Set(INPUT::GetOrhtoPosition());
+		targetTransform->m_position.Set(INPUT::GetOrhtoPosition());
+	
+	// By keyboard
+	if (INPUT::KeyTriggered(JE_ENTER))
+		targetTransform->m_position.Set(
+			Random::GetRandVec3(-350.f, -250.f, 0.f, 350.f, 250.f, 0.f));
 
-	if (GetOwner()->HasComponent<Transform>())	{
-		Transform* transform = GetOwner()->GetComponent<Transform>();
+	// Calculate the force to add
+	Calculate();
 
-		velocity += Flee(targetTransform->m_position).GetNormalize() * _dt * maxForce;
+	// Add force to velocity
+	vec3 acceleration = steeringForce.GetNormalize() / mass;
+	velocity += acceleration * _dt * maxSpeed;
 
-		velocity.Truncate(maxSpeed);
-		velocity.z = 0.f;
+	// Limit the velocity magnitude
+	Truncate(velocity, maxSpeed);
 
-		transform->m_rotation = vec3::UNIT_X.GetAngle(velocity);
-		transform->m_position += velocity * _dt;
+	// Update position
+	vec3 toAdd = velocity * _dt;
+	toAdd.z = 0.f;
+	m_transform->m_position += toAdd;
+	m_transform->m_rotation = vec3::UNIT_X.GetAngle(velocity);
 
-		ControlPosition(transform->m_position);
+	if (velocity.GetLengthSq() > 0.00000001) {
+		heading = velocity.GetNormalize();
+		side = GetPerpendicular(heading);
 	}
 
-	//Calculate();
-
-	//vec3 acceleration = steeringForce.GetNormalize() / mass;
-
-	//velocity += acceleration * _dt;
-
-	//velocity.Truncate(maxSpeed);
-
-	//// Update position
-	//if (GetOwner()->HasComponent<Transform>()) {
-
-	//	vec3 toAdd = velocity * _dt;
-	//	toAdd.z = 0.f;
-	//	GetOwner()->GetComponent<Transform>()->m_position += toAdd;
-	//}
-
-	//if (velocity.GetLengthSq() > 0.00000001) {
-	//	heading = velocity.GetNormalize();
-	//	side = heading.Perpendicular();
-	//}
+	ControlPosition(m_transform->m_position);
 }
 
 void Steering::Close()
